eb35ca2833b4bf0e7b9bee15004ff3b9ea4f78f2
youssef Tarek <yousef.tarek095@gmail.com>
2024-06-19 21:12:00 +0300

    added basics

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,8 @@
+number1 = 10
+number2 = 5
+sum = number1 + number2
+differance = number1 - number2
+product = number1 * number2
+print("Addition of", number1, "and", number2, "is", sum,
+      "Subtarction of", number1, "and", number2, "is", differance,
+      "multiplication of", number1, "and", number2, "is", product)
