6b740b745f2b3713cadb775c0e04ba4dfa1d1fc4
youssef Tarek <yousef.tarek095@gmail.com>
2024-07-05 20:39:40 +0300

    second task

---
+++ fns_and_dsa/hopping_list_manager.py
@@ -0,0 +1,28 @@
+
+def display_menu():
+    print("Shopping List Manager")
+    print("1. Add Item")
+    print("2. Remove Item")
+    print("3. View List")
+    print("4. Exit")
+def main():
+    shopping_list = []
+    while True:
+        display_menu()
+        choice = input("Enter your choice: ")
+
+        if choice == '1':
+            added_item = input("what item you want to add?")
+            shopping_list.append(added_item)
+        elif choice == '2':
+            removed_item = input("what item you want to remove?")
+            shopping_list.remove(removed_item)
+        elif choice == '3':
+            print(shopping_list)
+        elif choice == '4':
+            print("Goodbye!")
+            break
+        else:
+            print("Invalid choice. Please try again.")
+if __name__ == "__main__":
+    main()
