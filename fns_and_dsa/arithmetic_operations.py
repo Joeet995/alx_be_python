eaf20669b9f07bd052238dbe507780c8d27d06f1
youssef Tarek <yousef.tarek095@gmail.com>
2024-07-05 21:28:17 +0300

    first task

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -7,7 +7,7 @@ def perform_operation(num1, num2, operation):
  elif operation == "multiply":
   result = num1 * num2
  elif operation == "divide":
-  if num2 == 0 :
+  if num2 == 0:
    print("can not divied by zero")
   result = num1 / num2
  else: print("not a valid input")
