8a918cecbf2d71e5af67ccc7b27b94c15afb2dc8
youssef Tarek <yousef.tarek095@gmail.com>
2024-07-11 19:10:23 +0300

    task 00

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -1,16 +1,16 @@
 class BankAccount:
     def __init__(self, account_balance):
-        self.balance = account_balance
+        self.current_balance = account_balance
 
     def deposit(self, amount):
-        self.balance += amount
-        return self.balance
+        self.current_balance += amount
+        return self.current_balance
 
     def withdraw(self, amount):
-       if self.balance == 0:
+       if self.current_balance == 0:
            return False
        else:
            return True
 
     def display_balance(self):
-        print(f"Your current balane is {self.balance}")
+        print(f"Your current balane is {self.current_balance}")
