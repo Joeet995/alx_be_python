5e5ce7ef202bee896ef074b786e7874dd12cff32
youssef Tarek <yousef.tarek095@gmail.com>
2024-07-11 20:38:21 +0300

    tasks

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -14,4 +14,4 @@ class BankAccount:
            return True
 
     def display_balance(self):
-        print(f"Current Balance: ${self.current_balance}")
+        print(f"Current Balance: ${self.current_balance:.2f}")
