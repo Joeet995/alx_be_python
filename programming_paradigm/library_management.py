18acc41b53e461d6dfdff26b4aa78f4d83f29c99
youssef Tarek <yousef.tarek095@gmail.com>
2024-07-15 17:52:17 +0300

    last one

--- programming_paradigm/library_management.py
+++ programming_paradigm/library_management.py
@@ -5,33 +5,39 @@ class Book:
         self._is_checked_out = False
     
     def check_out(self):
+        if not self._is_checked_out:
             self._is_checked_out = True
         
     def return_book(self):
-            self._is_checked_out = False
+            if self._is_checked_out:
+                self._is_checked_out = False
     
-
+ 
 class Library(Book):
     def __init__(self):
         self._books = []
 
     def add_book(self, book):
         self._books.append(book)
-        self.return_book(book)
+        
         
         
     def list_available_books(self):
         for book in self._books:
-            print(f"{self.title} by {self.author}")
+            if book._is_checked_out == False:
+                print(f"{book.title} by {book.author}")
         
 
-    def check_out_book(self, book):
-        self._books.remove(book)
-        self.check_out(book)
+    def check_out_book(self, book_title):
+        for book in self._books:
+            if book_title == book.title and not book._is_checked_out:
+                book.check_out()
+        
 
-    def return_book(self, book):
-        self._books.append(book)
-        super().return_book(book)
+    def return_book(self, book_title):
+        for book in self._books:
+            if book_title == book.title and book._is_checked_out:
+                book.return_book()
 
 
     
